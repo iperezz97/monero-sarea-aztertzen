1ac393b68feb9ed2f32a999d3d3bf97d8d627bd2
iperezz97 <75305082+iperezz97@users.noreply.github.com>
2021-03-25 09:01:17 +0100

    Add files via upload

---
+++ src/osoa/nagusia.h
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <linux/kernel.h>
+#include <poll.h>
+#include <fcntl.h>
+
+#define B_RED "\033[1m\033[31m"
+#define RESET "\033[0m"
+
+
+// Levin header struct declaration
+struct levhdr {
+        __be64          sign;           // Signature of msg (8 bytes)   /  0x0121010101010101 (beti)
+        __le64          length;         // Length of msg (8 bytes)      /  0xf3=243
+        unsigned char   exp_resp;       // Expected response (1 byte)   /  0x01 (eskaera)
+        __le32          comm_cod;       // Command code (4 bytes)       /  0xe903=1001 (handshake), 0xef03=1007 (support flags)
+        __le32          retn_cod;       // Return code (4 bytes)        /  0x00000000
+        __le32          reserved;       // Reserved (4 bytes)           /  0x01000000
+        __be32          endchars;       // Ending chars (4 bytes)       /  0x01000000 (beti)
+} __attribute__((packed)); //33 byte-ak enpaketatuta
+
+// Methods + int main()
+void init(char *argv1, char *argv2);
+int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, unsigned int timeout_ms);
+
+//egiteko..
+void * jaso_transakzioak(void *);//
+
+
