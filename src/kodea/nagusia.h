c5feed4a8897f7585f6b317939efe0e8c055ba46
iperezz97 <75305082+iperezz97@users.noreply.github.com>
2021-06-01 08:34:36 +0200

    Update nagusia.h

--- src/kodea/nagusia.h
+++ src/kodea/nagusia.h
@@ -31,7 +31,5 @@ struct levhdr {
 void init(char *argv1, char *argv2);
 int connect_with_timeout(int sockfd, const struct sockaddr *addr, socklen_t addrlen, unsigned int timeout_ms);
 
-//egiteko..
-void * jaso_transakzioak(void *);//
 
 
